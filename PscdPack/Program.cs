67d9d74476f5ee0e3ea0eaae833670cb73e89f6f
Bastien Nocera <hadess@hadess.net>
2023-10-27 11:37:14 +0200

    Remove UI, and implement command-line unpacker instead
    As I don't have Windows.

--- PscdPack/Program.cs
+++ PscdPack/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Windows.Forms;
+using System.IO;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace PscdPack
 {
@@ -12,11 +13,62 @@ namespace PscdPack
         /// Program entry point.
         /// </summary>
         [STAThread]
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            var app = new CommandLineApplication
+            {
+                Name = Path.GetFileName(Environment.GetCommandLineArgs()[0]),
+                FullName = "PscdPack"
+            };
+
+            app.Command("unpack", (config) =>
+            {
+                config.FullName = "Unpacks/extracts .PAK file";
+                config.Description = "Unpacks/extracts SEGA Genesis Classics ROM";
+                var pathArg = config.Argument("path", "The path of the .PAK to unpack").IsRequired();
+
+                config.OnExecute(() =>
+                {
+                    PscdFormat pak;
+                    // FIXME check keybytes format to use
+                    pak = new PscdFormat(pathArg.Value, PscdFormat.ClassicsGoldKeyBytes);
+                    pak.Load();
+                    string newFileName = System.IO.Path.ChangeExtension(pathArg.Value, ".bin");
+                    using (var file = File.Open(newFileName, FileMode.Open))
+                    using (var rom = pak.GetImage())
+                    {
+                        rom.WriteTo(file);
+                        file.Flush();
+                    }
+                });
+
+                config.HelpOption();
+            });
+
+            app.VersionOptionFromAssemblyAttributes(System.Reflection.Assembly.GetExecutingAssembly());
+            app.HelpOption();
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 1;
+            });
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error while processing: {0}", ex);
+                return -1;
+            }
+
         }
 
     }
